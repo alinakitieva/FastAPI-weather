6d0334ab052922158f0640973a03cb9b1b4899d6
Alina <96070394+alinakitieva@users.noreply.github.com>
2022-02-14 17:28:08 +0300

    add settings

--- Project/app/database.py
+++ Project/app/database.py
@@ -2,7 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@db:5432/apidb"
+from app.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
