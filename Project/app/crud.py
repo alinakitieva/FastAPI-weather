1dbfe7743fe268a864d0e9feb7a5932835a71fa5
Alina <96070394+alinakitieva@users.noreply.github.com>
2022-02-14 17:27:46 +0300

    Update crud.py

--- Project/app/crud.py
+++ Project/app/crud.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import Session
 from datetime import datetime
 
-from app import models, schemas
+from app import models
 
 
-def get_temp(db: Session, country_code: str, city: str,
-             date_time: datetime):
+def get_weather_record(db: Session, country_code: str, city: str,
+                       date_time: datetime):
     return db.query(models.Weather).filter(
         models.Weather.country_code == country_code,
         models.Weather.city == city,
@@ -13,10 +13,10 @@ def get_temp(db: Session, country_code: str, city: str,
     ).first()
 
 
-def add_temp(db: Session, country_code: str, city: str,
-             date_time: datetime, temp: None):
+def add_weather_record(db: Session, country_code: str, city: str,
+                       date_time: datetime, temperature: float):
     db_weather = models.Weather(country_code=country_code, city=city,
-                                date_time=date_time, temp=temp)
+                                date_time=date_time, temperature=temperature)
     db.add(db_weather)
     db.commit()
     db.refresh(db_weather)
