0737a5d4b62c5ff92d65b6503c9113a615cf7279
Alina <96070394+alinakitieva@users.noreply.github.com>
2022-02-14 17:29:15 +0300

    Update models.py

--- Project/app/models.py
+++ Project/app/models.py
@@ -4,9 +4,9 @@ from app.database import Base
 
 
 class Weather(Base):
-    __tablename__ = "temp_info"
+    __tablename__ = "weathers"
 
     country_code = Column(String, primary_key=True)
     city = Column(String, primary_key=True)
     date_time = Column(DateTime, primary_key=True)
-    temp = Column(Float)
+    temperature = Column(Float)
